c5f54ae4667cf861a0ef4f300d5e7a7d33e0ebf0
tarousinsk1997 <tarousinsk1997@gmail.com>
2023-12-03 02:29:15 +0300

    update redis-storage

--- app/database/redis_storage.py
+++ app/database/redis_storage.py
@@ -6,7 +6,6 @@ import os
 REDIS_URL = 'redis://localhost:6379/0'
 redis_storage = RedisStorage.from_url(REDIS_URL, state_ttl=3600, data_ttl=30)
 redis_client = Redis()
-redis_storage = RedisStorage(redis_client)
 
 
 for key in redis_client.scan_iter('last_message:*'):
