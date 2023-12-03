b1c25e3eee67fabf62612b1dfce64f08a3b13be2
tarousinsk1997 <tarousinsk1997@gmail.com>
2023-12-03 03:28:37 +0300

    commit

--- app/database/redis_storage.py
+++ app/database/redis_storage.py
@@ -5,9 +5,9 @@ import os
 
 REDIS_URL = os.getenv('REDIS_URL')
 
-#redis_storage = RedisStorage.from_url(REDIS_URL, state_ttl=3600, data_ttl=30)
+redis_storage = RedisStorage.from_url(REDIS_URL, state_ttl=3600, data_ttl=30)
 redis_client = Redis(host='redis-storage')
-redis_storage = RedisStorage(redis_client)
+
 
 
 for key in redis_client.scan_iter('last_message:*'):
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
 import os
+import sys
 import time
 from aiohttp import web
 from datetime import datetime
 from aiogram import Dispatcher, Bot
+from aiogram.types import InputFile
 from bot_src.bot_init import bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from bot_src.middlewares.middleware import Last_Message_Middleware, My_MiddleWare
@@ -27,6 +29,42 @@ fn = datetime.now().strftime('logs/test.py-%Y%m%d%H%M%S.log')
 dp = Dispatcher(storage=redis_storage)
 
 
+
+
+
+
+
+
+
+
+
+# Webserver settings
+# bind localhost only to prevent any external access
+WEB_SERVER_HOST = os.getenv('DOCKER_LOCAL_HOST')
+# Port for incoming request from reverse proxy. Should be any available port
+WEB_SERVER_PORT = 8443
+
+# Path to webhook route, on which Telegram will send requests
+WEBHOOK_PATH = "/webhook"
+# Secret key to validate requests from Telegram (optional)
+#WEBHOOK_SECRET = "my-secret"
+# Base URL for webhook will be used to generate webhook URL for Telegram,
+# in this example it is used public address with TLS support
+#BASE_WEBHOOK_URL = "https://arguably-concise-stinkbug.ngrok-free.app"
+BASE_WEBHOOK_URL = os.getenv('BASE_WEBHOOK_URL') 
+
+WEBHOOK_URL = f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}"
+
+# Path to SSL certificate and private key for self-signed certificate.
+WEBHOOK_SSL_CERT = os.getenv('WEBHOOK_SSL_CERT')
+WEBHOOK_SSL_PRIV = os.getenv('WEBHOOK_SSL_PRIV')
+
+
+
+
+
+
+
 async def on_startup(bot: Bot) -> None:
     # If you have a self-signed SSL certificate, then you will need to send a public
     # certificate to Telegram
@@ -38,7 +76,10 @@ async def on_startup(bot: Bot) -> None:
     scheduler.start()
 
     # WEBHOOK
-    #await bot.set_webhook(WEBHOOK_URL)
+    await bot.set_webhook(WEBHOOK_URL, 
+                          certificate=InputFile(WEBHOOK_SSL_CERT),
+                          )
+
 
 
 
@@ -52,35 +93,6 @@ dp.update.middleware(Last_Message_Middleware())
 
 
 
-# Webserver settings
-# bind localhost only to prevent any external access
-WEB_SERVER_HOST = "127.0.0.1"
-# Port for incoming request from reverse proxy. Should be any available port
-WEB_SERVER_PORT = 8443
-
-# Path to webhook route, on which Telegram will send requests
-WEBHOOK_PATH = "/webhook"
-# Secret key to validate requests from Telegram (optional)
-#WEBHOOK_SECRET = "my-secret"
-# Base URL for webhook will be used to generate webhook URL for Telegram,
-# in this example it is used public address with TLS support
-BASE_WEBHOOK_URL = "https://arguably-concise-stinkbug.ngrok-free.app"
-
-WEBHOOK_URL = f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}"
-
-# Path to SSL certificate and private key for self-signed certificate.
-# WEBHOOK_SSL_CERT = "/path/to/cert.pem"
-# WEBHOOK_SSL_PRIV = "/path/to/private.key"
-
-
-
-
-
-
-async def print_hello():
-    print('WORKS')
-
-
 async def main():
 
     
@@ -91,21 +103,24 @@ async def main():
 
 # WEBHOOK
 
-    # app = web.Application()
-    # webhook_requests_handler = SimpleRequestHandler(
-    #     dispatcher=dp,
-    #     bot=bot)
-    # webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-    # setup_application(app, dp, bot=bot)
-    # app.add_routes(routes)
-    #web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot)
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    app.add_routes(routes)
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    #await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == "__main__":
 
     logging.getLogger('requests').setLevel(logging.WARNING)
-    logging.basicConfig(filename='/database/bot_log.log', level=logging.ERROR)
+    logging.basicConfig(
+                        stream=sys.stdout,
+                        #filename='/database/bot_log.log',  
+                        level=logging.DEBUG)
     asyncio.run(main())
     
